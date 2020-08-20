8f1f96fa2699ff4fb641b1c8b33e8b69841391e3
Mohcka <mikeduels@yahoo.com>
2020-08-19 20:11:38 -0700

    Adds home link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react"
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, Link } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
+
+import Container from "@material-ui/core/Container"
 
 import { AppContext, IAppContext, ApiServices } from "./context/AppContext"
 import Routes from "./routes"
@@ -17,6 +19,18 @@ const App: React.FC = () => {
       <AppContext.Provider value={globalAppValue}>
         {/* Layout */}
         <Router>
+          {/* Nav */}
+          <div
+            id="navbar"
+            style={{ borderBottom: "1px solid white", marginBottom: 10 }}
+          >
+            <Container>
+              <Link to="/">
+                <h2>Home</h2>
+              </Link>
+            </Container>
+          </div>
+          {/* Routes */}
           <Routes />
         </Router>
       </AppContext.Provider>
